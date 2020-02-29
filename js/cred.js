3376fd5f283a201a92e533ceb24c39da20a081d9
AYAN BISWAS <ayanbiswas184@gmail.com>
2020-02-29 11:20:38 +0530

    uploaded cred

---
+++ js/cred.js
@@ -0,0 +1,14 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDv9THv192xELZRoW9y4DS_o07_SdJeIS4",
+    authDomain: "bloodbank-online.firebaseapp.com",
+    databaseURL: "https://bloodbank-online.firebaseio.com",
+    projectId: "bloodbank-online",
+    storageBucket: "bloodbank-online.appspot.com",
+    messagingSenderId: "467225689984",
+    appId: "1:467225689984:web:ef4abcbbe1135704bc8b65"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
